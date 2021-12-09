d87ae8f3ecca6c42d4cc558bd06b57cfff467628
yopll <mrx.ga10@gmail.com>
2021-12-09 19:25:11 +0100

    DONE

---
+++ get_next_line_bonus.c
@@ -0,0 +1,104 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yopi <mrx.ga10@gmail.com>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/12/08 03:00:04 by yopi              #+#    #+#             */
+/*   Updated: 2021/12/09 19:23:12 by yopi             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+int	ft_checker(char *str)
+{
+	int	i;
+
+	i = 0;
+	if (!str)
+		return (-42);
+	while (str[i] != '\n' && str[i] != '\0')
+		i++;
+	if (str[i] == '\0')
+		return (-42);
+	return (i);
+}
+
+char	*ft_putline(char *keeper)
+{
+	char	*r_line;
+	int	size;
+
+	size = ft_checker(keeper);
+	if (!keeper)
+		return(NULL);
+	if (ft_checker(keeper) == -42)
+		return (ft_strdup(keeper));
+	r_line = (char *)malloc(sizeof(char) * (size + 2));
+	if (!r_line)
+		return (NULL);
+	ft_memmove(r_line, keeper, size);
+	r_line[size] = '\n';
+	r_line[size + 1] = '\0';
+	return (r_line);
+}
+
+char	*ft_backup(char *keeper)
+{
+	int	size;
+	int	len;
+
+
+	if(!keeper)
+		return (NULL);
+	len = ft_strlen(keeper);
+	size = ft_checker(keeper);
+	if (size  == -42 || (size - len) == -1)
+		return (free(keeper), NULL);
+	keeper = ft_substr(keeper, (size + 1), (len - size - 1));
+	return (keeper);
+}
+
+char	*get_next_line(int fd)
+{
+	static char	*keeper[1024];
+	char		*str;
+	int		r_read;
+	char		*line;
+
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (NULL);
+	str = malloc(sizeof(char) * (BUFFER_SIZE + 1));
+	if (!str)
+		return (NULL);
+	while (ft_checker(keeper[fd]) == -42)
+	{
+		r_read = read(fd, str, BUFFER_SIZE);
+		if (r_read <= 0)
+			break ;
+		str[r_read] = 0;
+		keeper[fd] = ft_strjoin(keeper[fd], str);
+	}
+	free(str);
+	line = ft_putline(keeper[fd]);
+	keeper[fd] = ft_backup(keeper[fd]);
+	return (line);
+
+}
+/*
+int main()
+{
+    int fd;
+    char *line;
+    int i;
+    i =0;
+    fd = open("test.txt", O_RDWR | O_CREAT);
+    //printf("the FD is : %d\n",fd);
+        line = get_next_line(fd);
+        printf("%s",line);
+        line = get_next_line(fd);
+        printf("%s",line);
+    return (0);
+}*/
---
+++ get_next_line_utils.c
@@ -0,0 +1,106 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yopi <mrx.ga10@gmail.com>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/12/08 03:01:39 by yopi              #+#    #+#             */
+/*   Updated: 2021/12/08 22:30:17 by yopi             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+size_t	ft_strlen(const char *str)
+{
+	size_t	i;
+
+	if (!str)
+		return (0);
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
+}
+
+char	*ft_strdup(const char *str)
+{
+	unsigned int	i;
+	size_t		len;
+	char		*save;
+
+	i = 0;
+	len = ft_strlen(str);
+	save = (char *)malloc(sizeof(char) * (len + 1));
+	if (!str)
+		return (NULL);
+	while (i < len)
+	{
+		save[i] = str[i];
+		i++;
+	}
+	save[i] = 0;
+	return (save);
+}
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	char	*str;
+	size_t	i;
+	size_t	length;
+
+	if (!s)
+		return (NULL);
+	length = ft_strlen(s);
+	if (start >= length)
+		return (ft_strdup(""));
+	if (len > ft_strlen(s + start))
+		len = ft_strlen(s + start);
+	str = malloc(sizeof(char) * (len + 1));
+	if (!str)
+		return (NULL);
+	i = 0;
+	while (i < len)
+	{
+		str[i] = s[start + i];
+		i++;
+	}
+	str[i] = 0;
+	free((char *)s);
+	return (str);
+}
+
+void	*ft_memmove(void *dst, const void *src, size_t len)
+{
+	size_t	i;
+
+	if (dst == src || len == 0)
+		return (dst);
+	i = -1;
+	if (dst > src)
+		while (len--)
+			((char *)dst)[len] = ((char *)src)[len];
+	else
+		while (++i < len)
+			((char *)dst)[i] = ((char *)src)[i];
+	return (dst);
+}
+
+char	*ft_strjoin(const char *s1, const char *s2)
+{
+	char	*str;
+	size_t	s1_len;
+	size_t	s2_len;
+
+	s1_len = ft_strlen(s1);
+	s2_len = ft_strlen(s2);
+	str = malloc(sizeof(char) * (s1_len + s2_len + 1));
+	if (!str)
+		return (NULL);
+	ft_memmove(str, s1, s1_len);
+	ft_memmove(str + s1_len, s2, s2_len);
+	str[s1_len + s2_len] = 0;
+	free((char *)s1);
+	return (str);
+}
